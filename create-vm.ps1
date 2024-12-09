3cecb4d34212afb907bced8c0d68e15a1e0a2676
William McQuaid <whmcquaid@gmail.com>
2024-12-09 15:21:59 -0800

    Add check for ssh keypair.

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,8 +1,14 @@
-# There should be a VM called webserver
+# There should be an ssh key pair
+if (test-path ./id_ed25519) {
+  echo 'ssh keypair already exists...'
+} else {
 
+}
+
+# There should be a VM called webserver
 if (multipass info webserver) {
   echo "VM webserver already exists"
 } else {
   echo "Creating webserver VM..."
-  multipass launch --name webserver
+  multipass launch --name webserver --bridged
 }
