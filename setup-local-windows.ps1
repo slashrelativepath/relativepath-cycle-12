642f3e25d77054855d9ba544ab75a1e0c9820cf3
William McQuaid <whmcquaid@gmail.com>
2024-12-10 15:33:31 -0800

    Update setup-local-windows.ps1

--- setup-local-windows.ps1
+++ setup-local-windows.ps1
@@ -3,6 +3,7 @@ if (get-command choco) {
 } else {
   echo 'installing choco...'
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 if (get-command nano) {
@@ -10,6 +11,7 @@ if (get-command nano) {
 } else {
   echo 'installing nano...'
   choco install -y nano
+  Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 if (get-command git) {
@@ -17,6 +19,7 @@ if (get-command git) {
 } else {
   echo 'installing git...'
   choco install git.install -y --force --params "'GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf'"
+  Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 if (get-command virtualbox) {
@@ -24,6 +27,7 @@ if (get-command virtualbox) {
 } else {
   echo 'installing virtualbox...'
   choco install -y virtualbox
+  Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 if (get-command multipass) {
@@ -31,6 +35,7 @@ if (get-command multipass) {
 } else {
   echo 'installing multipass...'
   choco install -y multipass --params="'/HyperVisor:VirtualBox'"
+  Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1; refreshenv
 }
 
 
