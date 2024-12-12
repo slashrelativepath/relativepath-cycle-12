73f3569c501e3a93308df16db241a6ceced9b2ed
William McQuaid <whmcquaid@gmail.com>
2024-12-11 16:25:38 -0800

    Add ssh keys to powershell create vm script.

--- create-vm.ps1
+++ create-vm.ps1
@@ -2,13 +2,16 @@
 if (test-path ./id_ed25519) {
   echo 'ssh keypair already exists...'
 } else {
-
+  echo "Generating keys.."
+  ssh-keygen -t ed25519 -f './id_ed25519' -N `"`"
 }
+
+# There should be a cloud init file
 
 # There should be a VM called webserver
 if (multipass info webserver) {
   echo "VM webserver already exists"
 } else {
   echo "Creating webserver VM..."
-  multipass launch --name webserver --bridged
+  multipass launch --name webserver --cloud-init ./cloud-init.yaml --bridged
 }
